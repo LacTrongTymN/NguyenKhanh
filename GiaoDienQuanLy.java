9de316afb9edcb72f570481b31495c2cdbd178a5
LacTrongTymN <honguyenkhanh05@gmail.com>
2024-01-23 01:00:27 +0700

    quanlyhosobenhnhan

---
+++ GiaoDienQuanLy.java
@@ -0,0 +1,438 @@
+package benhan;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+public class GiaoDienQuanLy {
+    private JFrame frame;
+    private JTable table;
+    private DefaultTableModel model;
+    private JTextField searchTextField;
+    private JTextField addIdField, addNameField, addAgeField, addGenderField, addPhoneNumberField, addHealthStatusField;
+    private JTextField editIdField, editNameField, editAgeField, editGenderField, editPhoneNumberField, editHealthStatusField;
+  
+    public static void main(String[] args) {
+    	String serverName = "3306";
+        String databaseName = "QLBENHAN";
+        String userName = "anhlonx";
+        String password = "Hnk180705@";
+
+        // URL kết nối cho MySQL
+        String url = "jdbc:mysql://localhost:3306/QLBENHAN";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("Không thể tìm thấy driver MySQL.");
+            return;
+        }
+        try (Connection connection = DriverManager.getConnection(url, userName, password)) {
+            System.out.println("Kết nối đến cơ sở dữ liệu thành công!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Không thể kết nối đến cơ sở dữ liệu!");
+        }
+        SwingUtilities.invokeLater(view::new);
+    }
+    
+    private void luutruhoso() {
+        // Kết nối đến MySQL
+        String connectionUrl = "jdbc:mysql://localhost:3306/QLBENHAN";
+        String userName = "anhlonx";
+        String password = "Hnk180705@";
+
+        try (Connection connection = DriverManager.getConnection(connectionUrl, userName, password)) {
+            // Câu truy vấn SQL để lấy dữ liệu từ bảng Patients
+            String sql = "SELECT * FROM Patients";
+
+            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                // Thực hiện câu truy vấn SELECT
+                var resultSet = preparedStatement.executeQuery();
+
+                // Xóa dữ liệu hiện tại trên bảng
+                model.setRowCount(0);
+
+                // Duyệt qua kết quả và thêm vào bảng
+                while (resultSet.next()) {
+                    String patientID = resultSet.getString("PatientID");
+                    String name = resultSet.getString("Name");
+                    String age = resultSet.getString("Age");
+                    String gender = resultSet.getString("Gender");
+                    String phoneNumber = resultSet.getString("PhoneNumber");
+                    String healthStatus = resultSet.getString("HealthStatus");
+
+                    model.addRow(new Object[]{patientID, name, age, gender, phoneNumber, healthStatus});
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(frame, "Lỗi khi lấy dữ liệu từ cơ sở dữ liệu.");
+        }
+    }
+    public GiaoDienQuanLy() {
+        initialize();
+     // Thêm dòng sau khi khởi tạo model trong phương thức initialize()
+        model = (DefaultTableModel) table.getModel();
+
+        // Gọi phương thức để tải dữ liệu từ MySQL khi khởi động ứng dụng
+        luutruhoso();
+    }
+
+    private void initialize() {
+        frame = new JFrame();
+
+        frame.setBounds(100, 100, 800, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(new BorderLayout());
+
+        JPanel functionPanel = new JPanel(new GridBagLayout());
+        functionPanel.setBackground(new Color(0x00FF7F));
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(5, 5, 5, 5);
+
+        JButton addButton = new JButton("thêm bệnh nhân");
+        JButton removeButton = new JButton("xóa bệnh nhân");
+        JButton searchButton = new JButton("tìm kiếm");
+        JButton editButton = new JButton("sửa thông tin bệnh nhân");
+
+        searchTextField = new JTextField(15);
+        addIdField = new JTextField(10);
+        addNameField = new JTextField(10);
+        addAgeField = new JTextField(10);
+        addGenderField = new JTextField(10);
+        addPhoneNumberField = new JTextField(10);
+        addHealthStatusField = new JTextField(10);
+
+        editIdField = new JTextField(10);
+        editNameField = new JTextField(10);
+        editAgeField = new JTextField(10);
+        editGenderField = new JTextField(10);
+        editPhoneNumberField = new JTextField(10);
+        editHealthStatusField = new JTextField(10);
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        functionPanel.add(addButton, gbc);
+
+        gbc.gridy = 1;
+        functionPanel.add(removeButton, gbc);
+
+        gbc.gridy = 10;
+        functionPanel.add(searchButton, gbc);
+
+        gbc.gridy =11;
+        functionPanel.add(searchTextField, gbc);
+
+        gbc.gridy = 4;
+        functionPanel.add(new JLabel("Số ID bệnh nhân:"), gbc);
+        gbc.gridx = 1;
+        functionPanel.add(addIdField, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 5;
+        functionPanel.add(new JLabel("Họ tên:"), gbc);
+        gbc.gridx = 1;
+        functionPanel.add(addNameField, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 6;
+        functionPanel.add(new JLabel("Tuổi:"), gbc);
+        gbc.gridx = 1;
+        functionPanel.add(addAgeField, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 7;
+        functionPanel.add(new JLabel("Giới tính:"), gbc);
+        gbc.gridx = 1;
+        functionPanel.add(addGenderField, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 8;
+        functionPanel.add(new JLabel("SĐT:"), gbc);
+        gbc.gridx = 1;
+        functionPanel.add(addPhoneNumberField, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 9;
+        functionPanel.add(new JLabel("Tình trạng sức khỏe:"), gbc);
+        gbc.gridx = 1;
+        functionPanel.add(addHealthStatusField, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        functionPanel.add(editButton, gbc);
+
+        // Add addButton and removeButton only once
+        GridBagConstraints gbcAddButton = new GridBagConstraints();
+        gbcAddButton.fill = GridBagConstraints.HORIZONTAL;
+        gbcAddButton.insets = new Insets(5, 5, 5, 5);
+
+        gbcAddButton.gridx = 0;
+        gbcAddButton.gridy = 0;
+        functionPanel.add(addButton, gbcAddButton);
+
+        GridBagConstraints gbcRemoveButton = new GridBagConstraints();
+        gbcRemoveButton.fill = GridBagConstraints.HORIZONTAL;
+        gbcRemoveButton.insets = new Insets(5, 5, 5, 5);
+
+        gbcRemoveButton.gridx = 0;
+        gbcRemoveButton.gridy = 1;
+        functionPanel.add(removeButton, gbcRemoveButton);
+
+        frame.getContentPane().add(functionPanel, BorderLayout.WEST);
+
+        JPanel tablePanel = new JPanel(new BorderLayout());
+        JScrollPane scrollPane = new JScrollPane();
+
+        table = new JTable();
+        table.setBackground(new Color(1.0f, 0.75f, 0.80f));
+        table.setModel(new DefaultTableModel(
+                new Object[][] {},
+                new String[] {"Số ID bệnh nhân", "Họ tên", "Tuổi", "Giới tính", "SĐT", "Tình trạng sức khỏe"}
+        ));
+        scrollPane.setViewportView(table);
+
+        tablePanel.add(scrollPane, BorderLayout.CENTER);
+        frame.getContentPane().add(tablePanel, BorderLayout.CENTER);
+
+        model = (DefaultTableModel) table.getModel();
+
+        addButton.addActionListener(e -> addPatient());
+        removeButton.addActionListener(e -> removePatient());
+        searchButton.addActionListener(e -> searchPatient());
+        editButton.addActionListener(e -> editPatient());
+
+        frame.setVisible(true);
+    }
+
+    	private void addPatient() {
+    	    String patientID = addIdField.getText();
+    	    String name = addNameField.getText();
+    	    String age = addAgeField.getText();
+    	    String gender = addGenderField.getText();
+    	    String phoneNumber = addPhoneNumberField.getText();
+    	    String healthStatus = addHealthStatusField.getText();
+
+    	    if (!patientID.isEmpty() && !name.isEmpty() && !age.isEmpty() &&
+    	        !gender.isEmpty() && !phoneNumber.isEmpty() && !healthStatus.isEmpty()) {
+    	        model.addRow(new Object[]{patientID, name, age, gender, phoneNumber, healthStatus});
+    	        
+    	        // Gọi hàm để chèn dữ liệu vào MySQL
+    	        insertpatient(patientID, name, age, gender, phoneNumber, healthStatus);
+
+    	        addIdField.setText("");
+    	        addNameField.setText("");
+    	        addAgeField.setText("");
+    	        addGenderField.setText("");
+    	        addPhoneNumberField.setText("");
+    	        addHealthStatusField.setText("");
+    	    } else {
+    	        JOptionPane.showMessageDialog(frame, "Vui lòng điền đầy đủ thông tin bệnh nhân.");
+    	    }
+    	}
+    	private void deletePatientFromDatabase(String patientID) {
+    	    // Kết nối đến MySQL
+    	    String connectionUrl = "jdbc:mysql://localhost:3306/QLBENHAN";
+    	    String userName = "anhlonx";
+    	    String password = "Hnk180705@";
+
+    	    try (Connection connection = DriverManager.getConnection(connectionUrl, userName, password)) {
+    	        // Câu truy vấn SQL để xóa bệnh nhân
+    	        String sql = "DELETE FROM Patients WHERE PatientID = ?";
+
+    	        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+    	            // Đặt giá trị cho tham số trong câu truy vấn DELETE
+    	            preparedStatement.setString(1, patientID);
+
+    	            // Thực hiện câu truy vấn DELETE
+    	            int rows = preparedStatement.executeUpdate();
+
+    	            // Kiểm tra xem có bản ghi nào bị xóa không
+    	            if (rows > 0) {
+    	                System.out.println("Đã xóa bệnh nhân khỏi cơ sở dữ liệu.");
+    	            } else {
+    	                System.out.println("Không có bệnh nhân nào được xóa.");
+    	            }
+    	        }
+    	    } catch (SQLException e) {
+    	        e.printStackTrace();
+    	        System.out.println("Lỗi khi xóa bệnh nhân khỏi cơ sở dữ liệu!");
+    	        System.out.println("SQL State: " + e.getSQLState());
+    	        System.out.println("Error Code: " + e.getErrorCode());
+    	        System.out.println("Error Message: " + e.getMessage());
+    	    }
+    	}
+
+    	private void removePatient() {
+    	int selectedRow = table.getSelectedRow();
+        if (selectedRow != -1) {
+            // Lấy giá trị Số ID bệnh nhân từ dòng đã chọn
+            String patientID = model.getValueAt(selectedRow, 0).toString();
+
+            // Gọi phương thức để xóa bệnh nhân từ MySQL
+            deletePatientFromDatabase(patientID);
+
+            // Xóa dòng từ bảng
+            model.removeRow(selectedRow);
+        } else {
+            JOptionPane.showMessageDialog(frame, "Vui lòng chọn bệnh nhân cần xóa.");
+        }
+    	}
+
+    	private void searchPatient() {
+        String timkiem = searchTextField.getText().trim().toLowerCase();
+        DefaultTableModel searchModel = new DefaultTableModel(
+                new Object[][] {},
+                new String[] {"Số ID bệnh nhân", "Họ tên", "Tuổi", "Giới tính", "SĐT", "Tình trạng sức khỏe"}
+        );
+
+        for (int i = 0; i < model.getRowCount(); i++) {
+            String patientID = model.getValueAt(i, 0).toString().toLowerCase();
+            String name = model.getValueAt(i, 1).toString().toLowerCase();
+            String age = model.getValueAt(i, 2).toString().toLowerCase();
+            String gender = model.getValueAt(i, 3).toString().toLowerCase();
+            String phoneNumber = model.getValueAt(i, 4).toString().toLowerCase();
+            String healthStatus = model.getValueAt(i, 5).toString().toLowerCase();
+
+            if (patientID.contains(timkiem) || name.contains(timkiem) || age.contains(timkiem) ||
+                gender.contains(timkiem) || phoneNumber.contains(timkiem) || healthStatus.contains(timkiem)) {
+                searchModel.addRow(new Object[]{patientID, name, age, gender, phoneNumber, healthStatus});
+            }
+        }
+
+        table.setModel(searchModel);
+    }
+    	private void insertpatient(String patientID, String name, String age, String gender, String phoneNumber, String healthStatus) {
+        // Kết nối đến MySQL
+        String connectionUrl = "jdbc:mysql://localhost:3306/QLBENHAN";
+        String userName = "anhlonx";
+        String password = "Hnk180705@";
+
+        try (Connection connection = DriverManager.getConnection(connectionUrl, userName, password)) {
+            // Câu truy vấn SQL để chèn dữ liệu
+            String sql = "INSERT INTO Patients (PatientID, Name, Age, Gender, PhoneNumber, HealthStatus) VALUES (?, ?, ?, ?, ?, ?)";
+
+            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                // Đặt giá trị cho các tham số trong câu truy vấn
+                preparedStatement.setString(1, patientID);
+                preparedStatement.setString(2, name);
+                preparedStatement.setString(3, age);
+                preparedStatement.setString(4, gender);
+                preparedStatement.setString(5, phoneNumber);
+                preparedStatement.setString(6, healthStatus);
+
+                // Thực hiện câu truy vấn INSERT
+                preparedStatement.executeUpdate();
+
+                // Hiển thị thông báo nếu chèn thành công
+                JOptionPane.showMessageDialog(frame, "Đã thêm bệnh nhân vào cơ sở dữ liệu.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(frame, "Lỗi khi thêm bệnh nhân vào cơ sở dữ liệu.");
+        }
+    }
+    	private void editPatient() {
+        int selectedRow = table.getSelectedRow();
+        if (selectedRow != -1) {
+            String patientID = model.getValueAt(selectedRow, 0).toString();
+            String name = model.getValueAt(selectedRow, 1).toString();
+            String age = model.getValueAt(selectedRow, 2).toString();
+            String gender = model.getValueAt(selectedRow, 3).toString();
+            String phoneNumber = model.getValueAt(selectedRow, 4).toString();
+            String healthStatus = model.getValueAt(selectedRow, 5).toString();
+
+            editIdField.setText(patientID);
+            editNameField.setText(name);
+            editAgeField.setText(age);
+            editGenderField.setText(gender);
+            editPhoneNumberField.setText(phoneNumber);
+            editHealthStatusField.setText(healthStatus);
+
+            JPanel panel = new JPanel(new GridLayout(7, 2));
+            panel.add(new JLabel("Số ID bệnh nhân:"));
+            panel.add(editIdField);
+            panel.add(new JLabel("Họ tên:"));
+            panel.add(editNameField);
+            panel.add(new JLabel("Tuổi:"));
+            panel.add(editAgeField);
+            panel.add(new JLabel("Giới tính:"));
+            panel.add(editGenderField);
+            panel.add(new JLabel("SĐT:"));
+            panel.add(editPhoneNumberField);
+            panel.add(new JLabel("Tình trạng sức khỏe:"));
+            panel.add(editHealthStatusField);
+
+            int result = JOptionPane.showConfirmDialog(null, panel, "Sửa thông tin bệnh nhân",
+                    JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+
+            if (result == JOptionPane.OK_OPTION) {
+                String newPatientID = editIdField.getText();
+                String newName = editNameField.getText();
+                String newAge = editAgeField.getText();
+                String newGender = editGenderField.getText();
+                String newPhoneNumber = editPhoneNumberField.getText();
+                String newHealthStatus = editHealthStatusField.getText();
+
+                if (!newPatientID.isEmpty() && !newName.isEmpty() && !newAge.isEmpty() &&
+                        !newGender.isEmpty() && !newPhoneNumber.isEmpty() && !newHealthStatus.isEmpty()) {
+                    model.setValueAt(newPatientID, selectedRow, 0);
+                    model.setValueAt(newName, selectedRow, 1);
+                    model.setValueAt(newAge, selectedRow, 2);
+                    model.setValueAt(newGender, selectedRow, 3);
+                    model.setValueAt(newPhoneNumber, selectedRow, 4);
+                    model.setValueAt(newHealthStatus, selectedRow, 5);
+
+                    // Cập nhật dữ liệu trong cơ sở dữ liệu
+                    updatepatient(patientID, newPatientID, newName, newAge, newGender, newPhoneNumber, newHealthStatus);
+                }
+            }
+        } else {
+            JOptionPane.showMessageDialog(frame, "Vui lòng chọn bệnh nhân cần sửa thông tin.");
+        }
+    }
+    	private void updatepatient(String oldPatientID, String newPatientID, String newName, String newAge, String newGender, String newPhoneNumber, String newHealthStatus) {
+        String connectionUrl = "jdbc:mysql://localhost:3306/QLBENHAN";
+        String userName = "anhlonx";
+        String password = "Hnk180705@";
+
+        try (Connection connection = DriverManager.getConnection(connectionUrl, userName, password)) {
+            // Câu lệnh SQL UPDATE
+            String query = "UPDATE Patients SET PatientID=?, Name=?, Age=?, Gender=?, PhoneNumber=?, HealthStatus=? WHERE PatientID=?";
+            //PreparedStatement giúp tăng hiệu suất của ứng dụng bởi vì câu truy vấn chỉ được biên dịch một lần.
+            try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+                // Đặt giá trị cho các tham số trong câu lệnh UPDATE
+                preparedStatement.setString(1, newPatientID);
+                preparedStatement.setString(2, newName);
+                preparedStatement.setString(3, newAge);
+                preparedStatement.setString(4, newGender);
+                preparedStatement.setString(5, newPhoneNumber);
+                preparedStatement.setString(6, newHealthStatus);
+                preparedStatement.setString(7, oldPatientID);
+
+                // Thực hiện câu lệnh UPDATE
+                int rows = preparedStatement.executeUpdate();
+
+                // Kiểm tra xem có bản ghi nào bị ảnh hưởng không
+                if (rows > 0) {
+                    System.out.println("Đã cập nhật thông tin bệnh nhân trong cơ sở dữ liệu.");
+                } else {
+                    System.out.println("Không có bệnh nhân nào được cập nhật.");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Lỗi khi cập nhật thông tin bệnh nhân trong cơ sở dữ liệu!");
+            System.out.println("SQL State: " + e.getSQLState());
+            System.out.println("Error Code: " + e.getErrorCode());
+            System.out.println("Error Message: " + e.getMessage());
+        }
+    }
+}
---
+++ QuanLyDangNhap.java
@@ -0,0 +1,185 @@
+package benhan;
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class QuanLyDangNhap extends JFrame implements ActionListener {
+
+    private Container c;
+    private JLabel title;
+    private JLabel email;
+    private JTextField temail;
+    private JLabel password;
+    private JPasswordField tpass;
+    private JLabel specialization;
+    private JComboBox<String> tspecial;
+    private JLabel term;
+    private JCheckBox tterm;
+    private JButton submit;
+    private JButton reset;
+    private JTextArea tout;
+    private JLabel res;
+
+    private String chuyenNganh[] = {"Khoa Nội", "Khoa Ngoại", "Khoa Răng Hàm Mặt", "Khoa Ung Bứu", "Khoa Thần Kinh"};
+
+    public QuanLyDangNhap() {
+        setTitle("Quản Lý Đăng Nhập");
+        setBounds(300, 90, 500, 600);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+
+        c = getContentPane();
+        c.setLayout(null);
+
+        title = new JLabel("Quản Lý Hồ Sơ");
+        title.setFont(new Font("Verdana", Font.PLAIN, 30));
+        title.setSize(300, 30);
+        title.setLocation(150, 30);
+        c.add(title);
+
+        email = new JLabel("Email");
+        email.setFont(new Font("Arial", Font.PLAIN, 20));
+        email.setSize(100, 20);
+        email.setLocation(100, 150);
+        c.add(email);
+
+        temail = new JTextField();
+        temail.setFont(new Font("Arial", Font.PLAIN, 15));
+        temail.setSize(190, 20);
+        temail.setLocation(200, 150);
+        c.add(temail);
+
+        password = new JLabel("Mật khẩu");
+        password.setFont(new Font("Arial", Font.PLAIN, 20));
+        password.setSize(100, 20);
+        password.setLocation(100, 200);
+        c.add(password);
+
+        tpass = new JPasswordField();
+        tpass.setFont(new Font("Arial", Font.PLAIN, 15));
+        tpass.setSize(190, 20);
+        tpass.setLocation(200, 200);
+        c.add(tpass);
+
+        specialization = new JLabel("Chuyên ngành");
+        specialization.setFont(new Font("Arial", Font.PLAIN, 20));
+        specialization.setSize(150, 20);
+        specialization.setLocation(100, 300);
+        c.add(specialization);
+
+        tspecial = new JComboBox<>(chuyenNganh);
+        tspecial.setFont(new Font("Arial", Font.PLAIN, 15));
+        tspecial.setSize(150, 20);
+        tspecial.setLocation(250, 300);
+        c.add(tspecial);
+
+        term = new JLabel("Đồng ý với điều kiện và điều khoản.");
+        term.setFont(new Font("Arial", Font.PLAIN, 15));
+        term.setSize(250, 20);
+        term.setLocation(150, 400);
+        c.add(term);
+
+        tterm = new JCheckBox();
+        tterm.setFont(new Font("Arial", Font.PLAIN, 15));
+        tterm.setSize(20, 20);
+        tterm.setLocation(400, 400);
+        c.add(tterm);
+
+        submit = new JButton("Đăng nhập");
+        submit.setFont(new Font("Arial", Font.PLAIN, 15));
+        submit.setSize(100, 20);
+        submit.setLocation(150, 450);
+        submit.addActionListener(this);
+        c.add(submit);
+
+        reset = new JButton("Nhập lại");
+        reset.setFont(new Font("Arial", Font.PLAIN, 15));
+        reset.setSize(100, 20);
+        reset.setLocation(270, 450);
+        reset.addActionListener(this);
+        c.add(reset);
+
+        tout = new JTextArea();
+        tout.setFont(new Font("Arial", Font.PLAIN, 15));
+        tout.setSize(300, 400);
+        tout.setLocation(500, 100);
+        tout.setLineWrap(true);
+        tout.setEditable(false);
+        c.add(tout);
+
+        res = new JLabel("");
+        res.setFont(new Font("Arial", Font.PLAIN, 20));
+        res.setSize(500, 25);
+        res.setLocation(100, 500);
+        c.add(res);
+
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == submit) {
+            if (tterm.isSelected()) {
+                String email = temail.getText();
+                String password = String.valueOf(tpass.getPassword());
+
+                if (email.isEmpty() || password.isEmpty()) {
+                    res.setText("Điền đầy đủ vào ô trống.");
+                } else {
+                    if (validateLogin(email, password)) {
+                        res.setText("Đăng nhập thành công.");
+
+                        // Ẩn DoctorSignUpForm và hiển thị view
+                        this.setVisible(false);
+                        new view(); // Tạo và hiển thị giao diện view
+                    } else {
+                        res.setText("Email hoặc mật khẩu không đúng.");
+                    }
+                }
+            } else {
+                res.setText("Đồng ý với điều kiện và điều khoản.");
+            }
+        } else if (e.getSource() == reset) {
+            temail.setText("");
+            tpass.setText("");
+            tspecial.setSelectedIndex(0);
+            tterm.setSelected(false);
+            tout.setText("");
+            res.setText("");
+        }
+    }
+
+    private boolean validateLogin(String email, String password) {
+        String url = "jdbc:mysql://localhost:3306/QLDANGNHAP";
+        String userName = "anhlonx";
+        String dbPassword = "Hnk180705@";
+
+        try (Connection connection = DriverManager.getConnection(url, userName, dbPassword)) {
+            String query = "SELECT * FROM TKBACSI WHERE email=? AND password=?";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+                preparedStatement.setString(1, email);
+                preparedStatement.setString(2, password);
+
+                try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                    // Kiểm tra nếu có ít nhất một bản ghi trùng khớp
+                    return resultSet.next();
+                }
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        new DoctorSignUpForm();
+    }
+}
+
